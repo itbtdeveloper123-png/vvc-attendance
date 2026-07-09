4b4c331e4e8f0632dd14ea60e7babcac46199c69
itbtdeveloper123-png <itbtdeveloper123-png@users.noreply.github.com>
2026-07-09 11:26:15 +0700

    clean(workspace): remove all diagnostic, connection, and test PHP scripts

--- read_active_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-echo "<h3>Active Database Configuration on Server:</h3>";
-echo "DB_SERVER: " . DB_SERVER . "<br/>";
-echo "DB_NAME: " . DB_NAME . "<br/>";
-echo "DB_USERNAME: " . DB_USERNAME . "<br/>";
-echo "is_localhost: " . ($is_localhost ? 'TRUE' : 'FALSE') . "<br/>";
-?>
